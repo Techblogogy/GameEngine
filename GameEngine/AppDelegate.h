0faf9f189d1f4e40159e3045999f738e32684c99
Fedor Bobylev <fedorbobylev@gmail.com>
2014-11-23 15:53:53 +0200

    Initial Commit

---
+++ GameEngine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameEngine
+//
+//  Created by Fedor Bobylev on 11/23/14.
+//  Copyright (c) 2014 Fedor Bobylev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
